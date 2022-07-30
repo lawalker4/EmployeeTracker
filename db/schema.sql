5ab6a73fc4891c50dd7af4b9b2a56d1859a16d80
Ashley <lauraashleywalker4@gmail.com>
2022-07-30 19:49:01 -0400

    update seeds

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS business;
+CREATE DATABASE business;
+USE business;
+
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS department;
+
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL (10,2),
+  department_id INTEGER,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,39 @@
+INSER INTO department (name)
+VALUES
+    ('IT'),
+    ('Sales'),
+    ('Engineering'),
+    ('Customer Service'),
+    ('Data Science'),
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ('Cyber Secerity', 90000, 1),
+    ('IT Specialist', 70000, 1),
+    ('Customer Sales Solutions', 120000, 2),
+    ('Sales Rep', 70000, 2),
+    ('Data Engineer', 100000, 3),
+    ('Software Engineer', 120000, 3),
+    ('Call Center', 65000, 4),
+    ('Customer Service', 55000, 4),
+    ('Data Scientist', 100000, 5),
+    ('Data Engineer', 100000, 5);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES
+    ('Viserys', 'Targaryan', 2,2),
+    ('Robb', 'Stark', 5, 5),
+    ('Bran', 'Stark', 1, 1),
+    ('Ned', 'Stark', 1, 2),
+    ('Joffrey', 'Baratheon', 3,2),
+    ('Tommen', 'Baratheon', 1, 5),
+    ('Khal', 'Drogo', 4, 1),
+    ('Cersei', 'Lannister', 3, 2),
+    ('Jamie', 'Lanister', 2,5),
+    ('Jon', 'Snow', 5, 1),
+    ('Grey', 'Worm', 3, 1),
+    ('Sansa', 'Stark', 4, 4),
+    ('Davos', 'Seaworth', 4,5),
+    ('Margaery', 'Tyrell', 2, 4),
+    ('Tormund', 'Giantsbane', 3, 4),
+    ('Jorah', 'Mormont', 5, 4),
