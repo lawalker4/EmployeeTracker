9fdc849e3ccea2775e7587db247bca22bf93c62c
Ashley <lauraashleywalker4@gmail.com>
2022-07-30 20:11:27 -0400

    update server

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 const express = require('express');
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+
+require('dotenv').config()
+
+//connects to the database
+
+const connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: process.env.PASSWORD,
+        database: 'employee.tracker_db'
+    });
+    
+// connects to the mysql database
+
+
+
+
